4fe60e56e549540f576143dc707742e1f5f58f9c
Abhinav Sharma <abhinav.sharma@bullhorn.com>
2018-09-12 01:46:53 +0530

    Lightning Components Usages
    Lightning Components Usages

--- src/aura/myApp2/myApp2Controller.js
+++ src/aura/myApp2/myApp2Controller.js
@@ -1,7 +1,7 @@
 ({
     init : function(component, event, helper) {
         $A.createComponent("c:myCmp", {
-            myFacet: 43
+            myString: '43'
         }, function(cmp, state) {
             console.log(state); // SUCCESS
         })
